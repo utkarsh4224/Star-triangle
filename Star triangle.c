11182c1caae0256b5254ca71d7b4c54bc75d387d
Utkarsh gupta <44867999+utkarsh4224@users.noreply.github.com>
2022-01-13 12:39:57 +0530

    Add files via upload

---
+++ Star triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   int i, space, rows, star = 0;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i, star = 0) {
+      for (space = 1; space <= rows - i; ++space) {
+         printf("  ");
+      }
+      while (star != 2 * i - 1) {
+         printf("* ");
+         ++star;
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
